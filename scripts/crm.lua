2197b31091ef9f9bf8cf44889946f9b6ef7d82f5
alexey <Alexey Khabulyak>
2020-09-05 18:28:11 +0300

    add lua request

--- scripts/crm.lua
+++ scripts/crm.lua
@@ -18,4 +18,36 @@ function http_json_request(url, method, data)
     }
 end
 
+
+
+local direction = event:getHeader("Caller-Direction")
+if direction ~= 'inbound' then
+    return
+end
 freeswitch.consoleLog("NOTICE", event:serialize())
+
+local src = event:getHeader("Caller-Caller-ID-Number")
+local dst = event:getHeader("Caller-Destination-Number")
+local event = event:getHeader("Event-Name")
+local uuid = event:getHeader("variable_uuid")
+local data = {}
+data["uuid"] = uuid
+data["direction"] = 'inbound'
+local method = ""
+local base_url = "http://127.0.0.1:8000/api/"
+local url = ""
+if event = 'CHANNEL_HANGUP_COMPLETE' then
+    duration = event:getHeader("variable_duration")
+    status = event:getHeader("variable_hangup_cause")
+    method = 'PATCH'
+    url = base_url .. "update"
+    data["duration"] = duration
+    data["status"] = status
+else
+    method = 'POST'
+    url = base_url .. "create"
+    data["src"] = src
+    data["dst"] = dst
+end
+
+http_json_request(url, method, data)

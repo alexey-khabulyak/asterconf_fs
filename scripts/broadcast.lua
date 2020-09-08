577a9aba7c127bd02190e0f6c06d99d2260826cf
alexey <Alexey Khabulyak>
2020-09-08 20:19:39 +0300

    bc

---
+++ scripts/broadcast.lua
@@ -0,0 +1,23 @@
+local json = require "json"
+local api = freeswitch.API()
+
+local local_ip = api:executeString("eval $${local_ip_v4}")
+local reg_users = api:executeString("show registrations as json")
+
+local t = json.decode(text)
+
+for k,v in ipairs(t["rows"]) do
+    event:addHeader("proto", "sip");
+    event:addHeader("dest_proto", "sip");
+    event:addHeader("from", "freeswitch@" .. local_ip);
+    event:addHeader("from_full", "sip:freeswitch@" .. local_ip);
+    event:addHeader("to", "sip:" .. v["reg_user"] .. "@" .. v["network_ip"] .. v["network_port"]);
+    event:addHeader("subject", "sip:" .. v["reg_user"] .. "@" .. v["network_ip"].. v["network_port"]); 
+    event:addHeader("type", "text/html");
+    event:addHeader("hint", "the hint");
+    event:addHeader("replying", "true");
+    event:addHeader("sip_profile", "internal");
+    event:addBody("TEST MESSAGE");
+    event:fire();
+
+end

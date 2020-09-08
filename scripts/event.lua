7cdd4d4e9d75b8092fd4e3be5efff8ac4c589e32
alexey <Alexey Khabulyak>
2020-09-08 15:34:25 +0300

    send sms

--- scripts/event.lua
+++ scripts/event.lua
@@ -4,13 +4,12 @@ event:addHeader("proto", "sip");
 event:addHeader("dest_proto", "sip");
 event:addHeader("from", "freeswitch@10.34.134.14");
 event:addHeader("from_full", "sip:freeswitch@10.34.134.14");
-event:addHeader("to", "sip:1019@62.76.112.134");
-event:addHeader("subject", "sip:1019@62.76.112.134");
+event:addHeader("to", "sip:1019@10.134.134.8");
+event:addHeader("subject", "sip:1019@10.134.134.8");
 event:addHeader("type", "text/html");
 event:addHeader("hint", "the hint");
 event:addHeader("replying", "true");
 event:addHeader("sip_profile", "external");
 event:addBody("Hello from Seven Du! Have fun!");
- 
--- freeswitch.consoleLog("info", event:serialize());
+
 event:fire();
